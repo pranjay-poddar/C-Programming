08199ef576cba083f43fca76d289b6b83b33c7c0
Coursera Learner <nobody@nowhere.nul>
2019-05-21 00:00:14 +0000

    added 7 traingle

--- 03_code2/code2.c
+++ 03_code2/code2.c
@@ -38,7 +38,7 @@ int main(void) {
   numStars = printTriangle(4);
   printf("That triangle had %d total stars\n", numStars);
   //now print "Here is a triangle with height 7\n"
-  printf("Here is a triangle with heiht 7\n");
+  printf("Here is a triangle with height 7\n");
 
   //then call printTriangle, passing in 7, and assign the result to numStars
   numStars7 = printTriangle(7);
