80f31df7b21bc6117208408c3b454c46f49134ee
Coursera Learner <nobody@nowhere.nul>
2019-06-01 02:56:07 +0000

    retirement prolem

---
+++ 07_retirement/retirement.c
@@ -0,0 +1,42 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+typedef struct _retire_info {
+  int months;
+  double contribution;
+  double rate_of_return;
+} retire_info;
+
+double balanceUpdate (retire_info period,double balance, int age){
+  int n = period.months;
+  for (int i=1; i<= n; i++){
+    printf("Age %3d month %2d you have $%.21f\n",age/12,age%12,balance);
+    balance = balance*(1+period.rate_of_return)+period.contribution;
+    age++;
+  }
+  return balance;
+}
+void retirement (int startAge,
+		 double initial,
+		 retire_info working,
+		 retire_info retired){
+  double balanceAfterWorking = balanceUpdate(working,initial,startAge);
+  balanceUpdate(retired,balanceAfterWorking,startAge+working.months);
+}
+int main(){
+  retire_info Working;
+  Working.months = 489;
+  Working.contribution = 1000;
+  Working.rate_of_return = 0.045/12;
+
+  retire_info retired;
+  retired.months = 384;
+  retired.contribution = -4000;
+  retired.rate_of_return = 0.01/12;
+  // start conditions
+  int Age = 327;
+  double savings = 21345;
+  retirement(Age,savings,Working,retired);
+  return 0;
+}
+  
