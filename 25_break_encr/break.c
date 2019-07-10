4c51928d36277eaa3f76e997a86389cdb5eafb07
Coursera Learner <nobody@nowhere.nul>
2019-07-10 01:26:35 +0000

    hope to be right

--- 25_break_encr/break.c
+++ 25_break_encr/break.c
@@ -5,41 +5,50 @@
 
 int max_idx (int * array, int n){
   int largest = 0;
-  int max_idx = -1;
+  int max_id = -1;
   for (int i=0; i<n ; i++){
+    printf("%d ",array[i]);
     if ( array[i] > largest){
       largest = array[i];
-      max_idx = i;
+      max_id = i;
     }
   }
-  return max_idx;
+  printf("the largest is %d",largest);
+  return max_id;
 }
 void break_cypher(FILE * f){
   int c;
   int letter_count[ALPHABET_NUM] = {0};
-  while(c = fgetc(f) != EOF){
+  while((c = fgetc(f)) != EOF){
+    //    printf("inside the while%d %c\n",c,c);
     if (isalpha(c)){
-      printf("c is alpha and it is %d\n",c);
+      //      printf("c is alpha and it is %d\n",c);
       c = tolower(c);
-      printf("%d\n",c);
+      //      printf("%d\n",c);
       letter_count[c-97]+=1;
     }
   }
-  int idx = max_idx(letter_count, ALPHABET_NUM);
-  printf("%d\n",idx-4);
+  int n = ALPHABET_NUM;
+  int idx = max_idx(letter_count, n);
+  printf("%d\n",(idx-4+26)%26);
 }
 int main(int argc, char ** argv){
   if (argc != 2){
-    printf("Usage: break input-filename");
+    fprintf(stderr,"Usage: break input-filename\n");
     return EXIT_FAILURE;
   }
   FILE * f = fopen(argv[1],"r");
   if (f == NULL){
-    printf("could not open the file\n");
+    //    printf("could not open the file\n");
+    perror("could not open the file");
     return EXIT_FAILURE;
   }
   printf("before enter the fun\n");
   break_cypher(f);
-  printf("after\n");
+  if (fclose(f) != 0){
+    perror("could not close the input file!");
+    return EXIT_FAILURE;
+  }
+  //  printf("after\n");
   return EXIT_SUCCESS;
 }
