34530e71a536b4dffa66918d2dd1b03470696aca
Coursera Learner <nobody@nowhere.nul>
2019-08-01 19:57:41 +0000

    removing printing

--- c4prj2_input/future.c
+++ c4prj2_input/future.c
@@ -31,8 +31,8 @@ void future_cards_from_deck(deck_t * deck, future_cards_t * fc){
     //    print_hand(deck);
     for (int i=0; i < fc->n_decks ; i++){
       for (int j=0; j<fc->decks[i].n_cards;j++){
-	print_card(*(deck->cards[i]));
-	printf("\n");
+	//print_card(*(deck->cards[i]));
+	//printf("\n");
 	*(fc->decks[i].cards[j]) = *(deck->cards[i]);
       }
     }
