9726bd3747a56b7729b2f6a3558dfa4d6424c7e4
Coursera Learner <nobody@nowhere.nul>
2019-08-01 16:05:41 +0000

    addming counter with cursor

--- c4prj2_input/input.c
+++ c4prj2_input/input.c
@@ -7,15 +7,17 @@
 
 
 deck_t * hand_from_string(const char * str, future_cards_t * fc){
-  char hand [strlen(str)+1];
+  size_t L = strlen(str);
+  char hand [L+1];
   char * cursor = strcpy(hand, str);
-  printf("%lu\n",strlen(cursor));
-  printf("%s\n",cursor);
+  //printf("%lu\n",strlen(cursor));
+  //printf("%s\n",cursor);
   deck_t * dummy_deck = malloc(sizeof(* dummy_deck));
   char * card_end = NULL;
   dummy_deck->n_cards = 0;
   dummy_deck->cards = NULL;
-  while(*cursor != '\n'){
+  int k =0;
+  while((k<(L-1)) && (*cursor !='\n')){
     card_end = strchr(cursor,' ');/*
     if (card_end == NULL){
       card_end = strchr(cursor, '\n');
@@ -23,18 +25,22 @@ deck_t * hand_from_string(const char * str, future_cards_t * fc){
 				  */
     if (cursor == card_end){
       cursor++;
+      k++;
       // card_end = strchr(cursor, ' ');
     }
     else{
       // check for future card
       if (*cursor == '?'){
-	add_future_card(fc, atoi(cursor+1), add_empty_card(dummy_deck));
+	cursor++;
+	add_future_card(fc, atoi(cursor), add_empty_card(dummy_deck));
+	k += (card_end - cursor+1);
 	cursor = card_end + 1;
       }
       else{
 	//printf("%c %c\n",*cursor,*(cursor+1));
 	add_card_to(dummy_deck, card_from_letters(*cursor,*(cursor+1)));
 	cursor += 2;
+	k += 2;
       }
     }
   }
