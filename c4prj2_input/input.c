9bee631b607bf48256e74369ab4b6d7c667de4aa
Coursera Learner <nobody@nowhere.nul>
2019-08-01 15:38:37 +0000

    bla

--- c4prj2_input/input.c
+++ c4prj2_input/input.c
@@ -7,7 +7,7 @@
 
 
 deck_t * hand_from_string(const char * str, future_cards_t * fc){
-  char hand [100];
+  char hand [300];
   char * cursor = strcpy(hand, str);
   printf("%s\n",cursor);
   deck_t * dummy_deck = malloc(sizeof(* dummy_deck));
