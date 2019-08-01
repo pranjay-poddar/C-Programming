403a26f2f08c6e06acd4e08d9ed7c37178e4d595
Coursera Learner <nobody@nowhere.nul>
2019-08-01 18:26:36 +0000

    bla

--- c4prj2_input/future.c
+++ c4prj2_input/future.c
@@ -25,6 +25,7 @@ void add_future_card(future_cards_t * fc, size_t index, card_t * ptr){
 void future_cards_from_deck(deck_t * deck, future_cards_t * fc){
   if (deck->n_cards < fc->n_decks){
     printf("there is not enough cards\n");
+    exit(EXIT_FAILURE);
   }
   else{
     for (int i=0; i < fc->n_decks ; i++){
