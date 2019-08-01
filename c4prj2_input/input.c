926cf2ef3340122be326232e25ca8f3f84d0ea03
Coursera Learner <nobody@nowhere.nul>
2019-08-01 15:07:37 +0000

    bla

--- c4prj2_input/input.c
+++ c4prj2_input/input.c
@@ -14,10 +14,11 @@ deck_t * hand_from_string(const char * str, future_cards_t * fc){
   dummy_deck->n_cards = 0;
   dummy_deck->cards = NULL;
   while(*cursor != '\n'){
-    card_end = strchr(cursor,' ');
+    card_end = strchr(cursor,' ');/*
     if (card_end == NULL){
       card_end = strchr(cursor, '\n');
     }
+				  */
     if (cursor == card_end){
       cursor++;
       // card_end = strchr(cursor, ' ');
@@ -29,6 +30,7 @@ deck_t * hand_from_string(const char * str, future_cards_t * fc){
 	cursor = card_end + 1;
       }
       else{
+	//printf("%c %c\n",*cursor,*(cursor+1));
 	add_card_to(dummy_deck, card_from_letters(*cursor,*(cursor+1)));
 	cursor += 2;
       }
