08211045a37131b8cbc26893b0c69181dd760e21
Coursera Learner <nobody@nowhere.nul>
2019-08-01 19:55:41 +0000

    fuck this shti

--- c4prj2_input/future.c
+++ c4prj2_input/future.c
@@ -24,13 +24,16 @@ void add_future_card(future_cards_t * fc, size_t index, card_t * ptr){
 
 void future_cards_from_deck(deck_t * deck, future_cards_t * fc){
   if (deck->n_cards < fc->n_decks){
-    printf("there is not enough cards\n");
+    printf("there is not enough cards in the deck\n");
     exit(EXIT_FAILURE);
   }
   else{
+    //    print_hand(deck);
     for (int i=0; i < fc->n_decks ; i++){
       for (int j=0; j<fc->decks[i].n_cards;j++){
-	fc->decks[i].cards[j] = deck->cards[i];
+	print_card(*(deck->cards[i]));
+	printf("\n");
+	*(fc->decks[i].cards[j]) = *(deck->cards[i]);
       }
     }
   }
--- c4prj2_input/main.c
+++ c4prj2_input/main.c
@@ -11,11 +11,23 @@ int main (){
     fprintf(stderr,"could not open the fiel\n");
     return EXIT_FAILURE;
   }
+  FILE * f1 = fopen("d.txt","r");
+  if (f1 == NULL){
+    fprintf(stderr,"could not open the file\n");
+    return EXIT_FAILURE;
+  }
+  
   size_t n_hands =0;
+  size_t n_deck =0;
   future_cards_t * fc = malloc(sizeof(*fc));
+  future_cards_t * fc1 = malloc(sizeof(*fc));
   fc->decks = NULL;
   fc->n_decks = 0;
   deck_t ** hands_array = read_input(f, &n_hands, fc);
+  deck_t ** deck_array = read_input(f1, &n_deck, fc1);
+  future_cards_from_deck(deck_array[0],fc);
+  print_hand(hands_array[1]);
+  printf("\n");
   /*
   for (int i=0; i<n_hands; i++){
     print_hand(hands_array[i]);
